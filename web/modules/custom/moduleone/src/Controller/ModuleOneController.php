75f6e9a75763c245aaf03ac96b33093d3fe0988b
gscottqueen <scott.queen@bixal.com>
2019-02-07 15:19:53 -0500

    updating our module

--- web/modules/custom/moduleone/src/Controller/ModuleOneController.php
+++ web/modules/custom/moduleone/src/Controller/ModuleOneController.php
@@ -12,7 +12,7 @@ class ModuleOneController extends ControllerBase {
   public function content() {
     return array(
       '#type' => 'markup',
-      '#markup' => t('Hello world'),
+      '#markup' => '<div id="data-gov-react-app" />',
     );
   }
 }
