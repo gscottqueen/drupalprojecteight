01c014171cbd0642e8ebd4a56a98ce65cae5b3b7
gscottqueen <scott.queen@bixal.com>
2019-02-13 10:19:03 -0500

    updated our react-app with dates and sorting

--- web/modules/custom/datagov/app/data-gov-app/src/App.js
+++ web/modules/custom/datagov/app/data-gov-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import Threats from './Threats.js'
+import Threats from './Threats'
 
 class App extends Component {
 
@@ -10,16 +9,58 @@ class App extends Component {
 
     this.state = {
       threats: [],
+      disclaimer: '',
     };
   }
 
-  componentWillMount() {
-    fetch('https://api.fda.gov/food/enforcement.json?search=report_date:[20040101+TO+20131231]&limit=100')
+  componentDidMount() {
+    // dates for our query
+    const todaysDate = new Date()
+    const range = new Date().setDate(todaysDate.getDate() - 200)
+    const startDate = new Date(range)
+    
+    // format our dates for the api params
+    function formatDate(date) {
+      let d = date,
+          year = d.getFullYear(),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate();
+      
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+      return [year, month, day].join('');
+    }
+
+    const todaysDateFormated = formatDate(todaysDate)
+    const rangeDateFormated = formatDate(startDate)
+
+    fetch('https://api.fda.gov/food/enforcement.json?search=report_date:[' + rangeDateFormated + '+TO+' + todaysDateFormated + ']&limit=100')
       .then(response => response.json())
-      // .then(data => this.setState({ data }));
       .then(data => {
+
+        function parseDate(results) {
+          let resultList = [...results]
+          // our sort function
+          function compareDates(a, b) {
+            const dateA = a.report_date
+            const dateB = b.report_date
+
+            if (dateA > dateB) {
+              return -1
+            } 
+            if (dateA < dateB) {
+              return 1
+            }
+            return 0
+            }
+
+          const sortedDates = resultList.sort(compareDates)
+          return sortedDates
+        }
+
         this.setState({
-          threats: data.results,
+          disclaimer: data.meta.disclaimer,
+          threats: parseDate(data.results),
         })
       });
   }
@@ -30,6 +71,7 @@ class App extends Component {
         <div>
           <Threats threatData={this.state.threats} />
         </div>
+        <div className="disclaimer">{this.state.disclaimer}</div>
       </div>
     );
   }
--- web/modules/custom/datagov/app/data-gov-app/src/Threats.js
+++ web/modules/custom/datagov/app/data-gov-app/src/Threats.js
@@ -1,13 +1,29 @@
 import React from 'react'
 
+function parseDate(date) {
+  let year = date.substring(0,4)
+  let month = date.substring(4,6)
+  let day = date.substring(6,8)
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+  let UTCDate = new Date(Date.UTC(year, month, day))
+
+  const formatedDate = new Intl.DateTimeFormat('en-US', { 
+    year: 'numeric', 
+    month: 'long', 
+    day: '2-digit',
+    timeZone: 'UTC', 
+  }).format(UTCDate)
+
+  return formatedDate
+}
+
 function Threats(props) {
   const threats = props.threatData;
-  console.log(threats)
   const threatList = threats.map((threat, index) =>
-    <li className="threat-list--detail" key={threat.recall_number}>
-      <p className="threat-list--detail-state">{threat.state}</p>
-      <p className="threat-list--detail-city">{threat.city}</p>
-      <p className="threat-list--detail-type">{threat.product_type}</p>
+    <li className="threat-list--detail" key={index}>
+      <p className="threat-list--detail-state">{threat.city}, {threat.state}</p>
+      <p className="threat-list--detail-date">{parseDate(threat.report_date)}</p>
       <p className="threat-list--detail-description">{threat.product_description}</p>
       <p className="threat-list--detail-recall">{threat.reason_for_recall}</p>
     </li>
