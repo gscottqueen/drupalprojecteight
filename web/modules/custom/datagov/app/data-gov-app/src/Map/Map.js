f555481c207cab8ba29a10ef2ac7bdaf982c84f1
gscottqueen <scott.queen@bixal.com>
2019-02-22 14:31:40 -0500

    getting our map centered and a plot marked

--- web/modules/custom/datagov/app/data-gov-app/src/Map/Map.js
+++ web/modules/custom/datagov/app/data-gov-app/src/Map/Map.js
@@ -4,98 +4,159 @@ import {
   ZoomableGroup,
   Geographies,
   Geography,
-  // Markers,
-  // Marker,
+  Markers,
+  Marker,
 } from "react-simple-maps"
 
 import GeoJSON from './world-50m.json'
 
 const wrapperStyles = {
   width: "100%",
-  maxWidth: 980,
-  margin: "0 auto",
 }
+
+// const markers = [
+//   { markerOffset: -25, name: "Buenos Aires", coordinates: [-58.3816, -34.6037] },
+//   { markerOffset: -25, name: "La Paz", coordinates: [-68.1193, -16.4897] },
+//   { markerOffset: 35, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+//   { markerOffset: 35, name: "Santiago", coordinates: [-70.6693, -33.4489] },
+//   { markerOffset: 35, name: "Bogota", coordinates: [-74.0721, 4.7110] },
+//   { markerOffset: 35, name: "Quito", coordinates: [-78.4678, -0.1807] },
+//   { markerOffset: -25, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
+//   { markerOffset: -25, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
+//   { markerOffset: 35, name: "Paramaribo", coordinates: [-55.2038, 5.8520] },
+//   { markerOffset: 35, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
+//   { markerOffset: -25, name: "Caracas", coordinates: [-66.9036, 10.4806] },
+// ]
+
+const markers = [
+  { 
+    markerOffset: -25, 
+    name: "Centerville", 
+    coordinates: [ -95, 25 ] 
+  }
+]
+
+function parseZips(recalls) {
+
+  let recallsArray = recalls
+
+  let zips = recallsArray.map(function(recall) {
+    if (recall.postal_code === "" || recall.postal_code === undefined){
+      let index = recalls.indexOf(recall)
+      recalls.splice(index, 1)
+    } else {
+      return '&location=' + recall.postal_code
+    }
+  })
+
+  let batchZips = JSON.stringify(zips)
+  let stringZips = batchZips.replace(/[^a-z0-9&=-]/g, "")
+  return stringZips
+}
+
 
 class Map extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      recalls: props.recallData,
       geoCodes: [],
+      zips: parseZips(props.recallData),
     };
   }
 
-  componentDidMount(props) {
+  componentDidMount() {
 
-    // console.log('props', props.recallData)
-
-
-    // get our fda response
-    // console.log('geocodes', this.state.geoCodes)
-    // console.log('props', props.recallData)
-
-    // iterate through that and create a new array with just the zips
-
-    // sent the new zips array to mapquest
-    fetch('http://open.mapquestapi.com/geocoding/v1/address?key=agvvu4mpL1dwAO4yamqLSuMjhqKClQiz&location=92821-3652')
+    // send the new zips array to mapquest
+    fetch('http://open.mapquestapi.com/geocoding/v1/batch?&maxResults=1&key=agvvu4mpL1dwAO4yamqLSuMjhqKClQiz' + this.state.zips + '')
     .then(response => response.json())
     .then(data => {
-      // console.log(data)
       this.setState({
-        geoCodes: data.results[0],
+        geoCodes: data.results,
       })
-      // console.log('geocodes', this.state.geoCodes)
     });
   }
 
-  render(props) {
-    const recalls = props
-    console.log(recalls)
+  render() {
     return (
       <div style={wrapperStyles}>
         <ComposableMap
-          projectionConfig={{
-            scale: 205,
-            rotation: [-11,0,0],
-          }}
-          width={980}
-          height={551}
+          projectionConfig={{ scale: 1000 }}
+          width={1000}
+          height={1000}
           style={{
             width: "100%",
             height: "auto",
+            margin: "0px",
+            maxWidth: "100%",
           }}
           >
-          <ZoomableGroup center={[0,20]} disablePanning>
+          <ZoomableGroup center={[ -95, 25 ]} disablePanning>
             <Geographies geography={GeoJSON}>
-              {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (
-                <Geography
-                  key={i}
-                  geography={geography}
-                  projection={projection}
-                  style={{
-                    default: {
-                      fill: "#ECEFF1",
-                      stroke: "#607D8B",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: "#607D8B",
-                      stroke: "#607D8B",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#FF5722",
-                      stroke: "#607D8B",
-                      strokeWidth: 0.75,
-                      outline: "none",
-                    },
-                  }}
-                />
-              ))}
+              {(geographies, projection) =>
+                geographies.map((geography, i) =>
+                    <Geography
+                      key={i}
+                      geography={geography}
+                      projection={projection}
+                      style={{
+                        default: {
+                          fill: "#ECEFF1",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        hover: {
+                          fill: "#CFD8DC",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                        pressed: {
+                          fill: "#FF5722",
+                          stroke: "#607D8B",
+                          strokeWidth: 0.75,
+                          outline: "none",
+                        },
+                      }}
+                    />
+                  )
+              }
             </Geographies>
+            <Markers>
+              {markers.map((marker, i) => (
+                <Marker
+                  key={i}
+                  marker={marker}
+                  style={{
+                    default: { fill: "#FF5722" },
+                    hover: { fill: "#FFFFFF" },
+                    pressed: { fill: "#FF5722" },
+                  }}
+                  >
+                  <circle
+                    cx={0}
+                    cy={0}
+                    r={10}
+                    style={{
+                      stroke: "#FF5722",
+                      strokeWidth: 3,
+                      opacity: 0.9,
+                    }}
+                  />
+                  <text
+                    textAnchor="middle"
+                    y={marker.markerOffset}
+                    style={{
+                      fontFamily: "Roboto, sans-serif",
+                      fill: "#607D8B",
+                    }}
+                    >
+                    {marker.name}
+                  </text>
+                </Marker>
+              ))}
+            </Markers>
           </ZoomableGroup>
         </ComposableMap>
       </div>
